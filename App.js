b411a671c8ba8508a8e342441a1353b7f290a5e3
couoheduardohl <eduardo.couoh@hiumanlab.com>
2021-07-13 15:15:59 -0500

    * Expo * Nativebase

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {NativeBaseProvider, Box, Text} from 'native-base';
+import {theme} from "./proyect/theme";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NativeBaseProvider theme={theme}>
+            <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+                <Text color={'amber.400'}>Open up App.js to start working on your app!</Text>
+            </Box>
+        </NativeBaseProvider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {}
 });
