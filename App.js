c329a457c63455608752c2bbde6ded706cd98167
couoheduardohl <eduardo.couoh@hiumanlab.com>
2021-07-20 17:28:47 -0500

    * Implementación redux ducks. * Navegación.

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
-import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {NativeBaseProvider, Box, Text} from 'native-base';
-import {theme} from "./proyect/theme";
+import {theme} from "./theme";
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from "@react-navigation/native";
+import HomeScreen from "./screens/HomeScreen";
+import {Provider} from "react-redux";
+import {store} from "./redux/store";
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
     return (
+        <Provider store={store}>
         <NativeBaseProvider theme={theme}>
-            <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-                <Text color={'amber.400'}>Open up App.js to start working on your app!</Text>
-            </Box>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={HomeScreen}/>
+                </Stack.Navigator>
+            </NavigationContainer>
         </NativeBaseProvider>
+        </Provider>
     );
 }
+
 
 const styles = StyleSheet.create({
     container: {}
