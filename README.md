e61f38016119e54459a1f4ceb036339a5bc8bbe4
eduardocouoh <eduardo.couoh@hiumanlab.com>
2022-06-03 11:01:30 -0500

    * Implementacion EAS * Expo upgrade 45

--- README.md
+++ README.md
@@ -3,16 +3,8 @@ Implementaciones necesarias para iniciar una aplicaciones en React Native.
 
 ### Implementaciones ###
 
-* Expo 42
+* Expo 45
 * Nativebase 3
 * Reducks
 * Navegación
-
-### Requisitos para inicializar la app ###
-
-- Instalar Node JS
-- npm install --global expo-cli (desde terminal windows)
-- npm i (terminal en el  proyecto)
-- expo start
-- Instalar en el móvil Expo Go
-- Escanear el código QR
+* EAS
---
+++ app.config.js
@@ -0,0 +1,59 @@
+const commonConfig = {
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "splash": {
+        "image": "./assets/splash.png",
+        "resizeMode": "contain",
+        "backgroundColor": "#ffffff"
+    },
+    "updates": {
+        "fallbackToCacheTimeout": 0
+    },
+    "assetBundlePatterns": [
+        "**/*"
+    ],
+
+
+    "web": {
+        "favicon": "./assets/favicon.png"
+    }
+};
+
+const ios = {
+    "supportsTablet": true
+};
+
+const android = {
+    "adaptiveIcon": {
+        "foregroundImage": "./assets/adaptive-icon.png",
+        "backgroundColor": "#FFFFFF"
+    }
+};
+module.exports = () => {
+    if (process.env.APP_ENV === "ios") {
+        return {
+            ...commonConfig,
+            "name": "Expo React Native Template",
+            "slug": "Expo-React-Native-Template",
+            "version": "7.0.8",
+            "ios": ios
+        };
+    } else if (process.env.APP_ENV === "android") {
+        return {
+            ...commonConfig,
+            "name": "Expo React Native Template",
+            "slug": "baja-tu-seguro",
+            "version": "5.33",
+            "android": android
+        };
+    } else if (process.env.APP_ENV === "expo") {
+        return {
+            ...commonConfig,
+            "name": "Bajatuseguro (Development)",
+            "slug": "Expo-React-Native-Template",
+            "version": "1.0.0",
+            "ios": ios,
+            "android": android
+        };
+    }
+};
